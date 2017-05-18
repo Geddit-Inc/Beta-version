77bf2c2510772561e8b12b6c75df356ac4f37a57
Aaron Dai <mrvicadai@gmail.com>
2017-05-18 07:57:04 -0700

    cleanup routes

--- server/models/index.js
+++ server/models/index.js
@@ -1,20 +1,10 @@
-'use strict';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/App';
-
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.json')[env];
+var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
